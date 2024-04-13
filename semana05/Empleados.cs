1ac8a5cea06b20e42ea36e37c4df8379ce42ea2b
Alumno <Alumno@LAB1504-11>
2024-04-13 16:31:05 -0500

    Agregar archivos de proyecto.

---
+++ semana05/Empleados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana05
+{
+    public class Empleados
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string Tratamiento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaContratacion { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public string Region { get; set; }
+        public string CodPostal { get; set; }
+        public string Pais { get; set; }
+        public string TelDomicilio { get; set; }
+        public string Extension { get; set; }
+        public string Notas { get; set; }
+        public int Jefe { get; set; }
+        public decimal SueldoBasico { get; set; }
+    }
+
+}
---
+++ semana05/InsertarEmpleados.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace semana05
+{
+    public partial class InsertarEmpleados : Window
+    {
+        string connectionString = "Data Source=LAB1504-11\\SQLEXPRESS;Initial Catalog=NeptunoDB; ";
+
+        public InsertarEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void InsertarEmpleado_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                   
+                    connection.Open();
+
+                    
+                    using (SqlCommand command = new SqlCommand("USP_GetEmpleados", connection))
+                    {
+                        
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        
+                        command.Parameters.AddWithValue("@Apellidos", txtApellidos.Text);
+                        command.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        command.Parameters.AddWithValue("@Cargo", txtCargo.Text);
+                        command.Parameters.AddWithValue("@Tratamiento", txtTratamiento.Text);
+                        command.Parameters.AddWithValue("@FechaNacimiento", txtTratamiento.Text);
+                        command.Parameters.AddWithValue("@FechaContratacion", txtTratamiento.Text);
+                        command.Parameters.AddWithValue("@Ciudad", txtTratamiento.Text);
+                        command.Parameters.AddWithValue("@region", txtTratamiento.Text);
+
+
+
+
+
+
+
+                        command.ExecuteNonQuery();
+
+                        
+                        MessageBox.Show("Empleado insertado correctamente.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al insertar empleado: " + ex.Message);
+            }
+        }
+    }
+}
